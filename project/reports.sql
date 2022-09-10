8d939e66e62352b62d235e3e3aedec74d7e83531
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-09-10 17:19:20 -0300

    Fix identation try 1

--- project/reports.sql
+++ project/reports.sql
@@ -1,7 +1,7 @@
 -- Gerar uma lista com as conferências iniciadas nos últimos 30 dias, com o número de sessões em cada conferência. As conferências
 -- devem ser ordenadas por data em ordem crescente
 SELECT 
-	  c.id "ID da Conferência", 
+	c.id "ID da Conferência", 
     c.titulo 'Título da Conferência', 
     (SELECT COUNT(*) FROM sessao s WHERE s.id_conferencia = c.id) "Número de Sessões"
 FROM conferencia c    
@@ -11,7 +11,7 @@ ORDER BY c.data_inicio ASC;
   
 -- Gerar uma lista com as 10 sessões mais bem avaliadas. O título da conferência, assim como o nome do palestrante também devem ser apresentados
 SELECT
-	  s.id "ID da Sessão",
+	s.id "ID da Sessão",
     s.assunto "Assunto da Sessão",
     c.titulo "Conferência",
     p.nome "Nome do Palestrante",
@@ -26,7 +26,7 @@ LIMIT 10;
 -- Gerar uma lista dos 5 participantes que mais realizaram palestras na plaforma. Os resultados devem ser ordenados em ordem decrescente pelo número
 -- de palestras
 SELECT
-	  p.id "ID do Participante",
+	p.id "ID do Participante",
     p.nome "Nome do Participante",
     (SELECT COUNT(*) FROM participacao WHERE id_participante = p.id) "Participações"
  FROM participante p   
@@ -36,11 +36,11 @@ SELECT
 -- Gerar uma lista com todos os participantes que estiveram em alguma sessão nos últimos 790 dias. Ao lado do nome de cada participante
 -- deve ser apresentado se ele/ela gostou ou não da sessao. Caso sua avaliação tenha sido >= 3, significa que tenha gostado, caso contrário não.
 SELECT 
-	  p.id "ID do Participante",
+	p.id "ID do Participante",
     p.nome "Nome do Participante",
     CASE 
     	WHEN (SELECT a.nota FROM avaliacao a WHERE a.id_participante = p.id LIMIT 1) >= 3 THEN 'Sim'
-      ELSE 'Não'
+    ELSE 'Não'
     END 'Gostou'
 FROM participante p
 INNER JOIN participacao ON p.id = participacao.id_participante
