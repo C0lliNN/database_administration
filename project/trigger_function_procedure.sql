5d06464b87c82e14151cfa15e97d5665c72138a6
C0lliNN <rapphaelmanhaes2017@hotmail.com>
2022-09-27 19:49:26 -0300

    Trigger, Function and Procedure

---
+++ project/trigger_function_procedure.sql
@@ -0,0 +1,68 @@
+--  A plataforma decidiu realizar anúncios durante um evento de uma universidade. Assim, deseja-se saber 
+--  quão efetiva estão sendo as propagandas. Para esse fim, deverá ser criado um trigger registrando
+--  a data e horário de novas inscrições
+
+
+CREATE TABLE inscricoes_logs (
+  id INT NOT NULL PRIMARY KEY auto_increment,
+  id_sessao INT NOT NULL,
+  criado_em TIMESTAMP NOT NULL
+);
+
+delimiter #
+CREATE TRIGGER inscricoes_logs_trigger AFTER INSERT ON participacao
+FOR EACH ROW
+BEGIN
+  INSERT INTO inscricoes_logs VALUES(null, new.id, CURRENT_TIMESTAMP())
+END#
+delimiter ;
+
+-- A plataforma está tendo uma grande despesa para armazenar os dados. Como a tabela de avalicao 
+-- está muito grande e apenas a média das avaliações para uma determinada sessão é útil, 
+-- é necessário realizar a compilação dessa tabela da seguinte maneira:
+-- para cada sessao, obtenha a media das avaliações, 
+-- remove todas as avaliações para a sessao
+-- insera um registro com a media da sesao
+
+delimiter #
+CREATE PROCEDURE compilar_avaliacoes()
+BEGIN
+  DECLARE fim INT;
+  DECLARE id_sessao INT;
+  DECLARE media INT;
+  DECLARE curs CURSOR FOR SELECT DISTINCT id_sessao FROM avaliacao;
+  DECLARE CONTINUE HANDLER FOR NOT FOUND SET fim = 1;
+
+  START TRANSACTION;
+
+  OPEN curs;
+  REPEAT
+    FETCH curs INTO id_sessao;
+    SELECT ROUND(AVG(nota)) INTO media FROM avaliacao WHERE id_sessao = id_sessao;
+
+    DELETE FROM avaliacao WHERE id_sessao = id_sessao;
+    INSERT INTO avaliacao VALUES(null, id_sessao, 1, media);
+    
+  UNTIL fim END REPEAT;
+
+  CLOSE curs;
+
+  COMMIT;
+END#
+
+delimiter ;
+
+-- O uso da media das avaliações se tornou muito comum durante as operações do DBA. Assim uma
+-- função que recebesse o id de uma sessao e retorne a media como um inteiro seria muito útil
+delimiter #
+
+
+CREATE FUNCTION obter_media (id_sessao INT)
+RETURNS INT
+BEGIN
+  DECLARE media INT;
+  SELECT ROUND(AVG(nota)) INTO media FROM avaliacao WHERE id_sessao = id_sessao;
+  RETURN media;
+END#
+
+delimiter ;
